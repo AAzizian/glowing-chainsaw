6917077712e314858072c398f8d51eac761be3f3
Shervin <Shervin@SHERVIN-DESKTOP>
2019-08-28 18:30:52 +0430

    Initial

---
+++ ShoppingCart/ShoppingCart.Core/Contracts/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingCart.Core.Models;
+using ShoppingCart.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Core.Contracts
+{
+    public interface IOrderService
+    {
+        void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
+    }
+}
---
+++ ShoppingCart/ShoppingCart.WebUI.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart.WebUI.Controllers;
+using ShoppingCart.Core.Contracts;
+using ShoppingCart.Core.Models;
+using System.Web.Mvc;
+using ShoppingCart.Core.ViewModels;
+using System.Linq;
+
+namespace ShoppingCart.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCatgeoryContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+
+            HomeController controller = new HomeController(productContext, productCatgeoryContext);
+
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
+
+        }
+    }
+}
